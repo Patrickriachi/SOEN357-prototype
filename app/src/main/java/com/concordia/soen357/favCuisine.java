af8e2a4e170f2e9f6a849980dc397aa837ae3e0d
Patrick <patrickriachi123@gmail.com>
2024-04-13 21:05:04 -0400

    pages

--- app/src/main/java/com/concordia/soen357/favCuisine.java
+++ app/src/main/java/com/concordia/soen357/favCuisine.java
@@ -1,17 +1,18 @@
 package com.concordia.soen357;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class favCuisine extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fav_cuisine);
 
         // Find the button by its ID
         Button skipButton = findViewById(R.id.skipButton);
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start the favCuisine activity
-                Intent intent = new Intent(MainActivity.this, favCuisine.class);
+                Intent intent = new Intent(favCuisine.this, healthGoals.class);
                 startActivity(intent);
             }
         });
